ab937ba7abec8dfabb82fa89a0cb46ee1dc5cba7
Fiddy <85234199+Felixx2112@users.noreply.github.com>
2022-08-22 22:20:40 +0700

    combine api get full board, handle cors

--- src/services/board.service.js
+++ src/services/board.service.js
@@ -12,22 +12,23 @@ const createNew = async (data) => {
 const getFullBoard = async (boardId) => {
   try {
     const board = await BoardModel.getFullBoard(boardId);
-
     /*
         // Sort column bt columnOrder, sort cards by cardOrder,
         //but i think this step will pass to frontend dev to ease the burden on the server :v
     */
-
+    if (!board || !board.columns) {
+      throw new Error("Board not found!");
+    }
     // Add card to each column
     board.columns.forEach((column) => {
       column.cards = board.cards.filter(
         (card) => card.columnId.toString() === column._id.toString()
       );
     });
+    console.log(board);
 
     //remove cards from board
     delete board.cards;
-    console.log("board", board);
     return board;
   } catch (e) {
     throw new Error(e);
